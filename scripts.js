11c74ad9d720f39eccff2c3cb949dae946713658
EconClass <bizmail327@gmail.com>
2019-06-04 12:28:16 -0700

    added popup

--- scripts.js
+++ scripts.js
@@ -75,8 +75,6 @@ prev.addEventListener("click", function() {
   dotClick('second', 'third', 'first');
 })
 
-////////////////// end of testimonial section
-
 
 /////////////FAQ accordion section///////////////
 
@@ -105,6 +103,37 @@ for (let i = 0; i < acc.length; i++) {
     }
   });
 }
+
+
+/////////popup//////////////
+
+//declare default vars
+const popUp = document.getElementById('pop-up');
+const popUpLayer = document.getElementById('pop-up-layer');
+const closeModal = document.getElementById('close-pop-up');
+
+//nav-bar button
+const subBtn = document.getElementById('sub-btn');
+
+//navbar button click makes the popup appear
+subBtn.addEventListener("click", () => {
+  popUpLayer.style.display = 'flex';
+})
+
+//close model click makes the popup disappear
+closeModal.addEventListener("click", function() {
+  popUpLayer.style.display = 'none';
+})
+
+let popUpSeconds = 40;
+
+//trigger popup after seconds variable
+let popUpFun = window.setInterval(function() {
+  popUpLayer.style.display = 'flex';
+
+  window.clearInterval(popUpFun);
+
+}, popUpSeconds * 1000);
 
 // const data = [
 //     {url:"http://llll", name:'hello'}
